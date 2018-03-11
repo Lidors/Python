313850b31be823466416e22ed3bd9090376548d6
Lidors <37260959+Lidors@users.noreply.github.com>
2018-03-11 10:40:55 +0200

    Update ex1.3.py

--- ex1.3.py
+++ ex1.3.py
@@ -1,3 +1,4 @@
+
 sun_class = [['Name', 'Kandel', 'Rafi', 'Sason', 'Akon'],
              ['Grade A', 60, 90, 100, 80], ['Grade B', 70, 95, 20, 80]]
 
@@ -9,13 +10,14 @@ linear_algebra4 = [['Name', 'Kandel', 'Hodgkin', 'Huxley', 'Akon'],
 #print(linear_algebra4)
 
 
-def compare_subjects_within_student(subj1_all_students, subj2_all_students):
-      """
-    Compare the two subjects with their students and print out the "preferred"
+def compare_subjects_within_student(sun_class, linear_algebra4):
+  """
+  Compare the two subjects with their students and print out the "preferred"
     subject for each student. Single-subject students shouldn't be printed.
 
-    Choice for the data structure of the function's arguments is up to you.
-    """
+  Choice for the data structure of the function's arguments is up to you.
+  """
+
   max_grade1 =[]
   max_grade1.append(sun_class[0][1:])
   var = max(sun_class[1][1:], sun_class[2][1:])
@@ -36,8 +38,9 @@ def compare_subjects_within_student(subj1_all_students, subj2_all_students):
 			  finel_list[j] = 'sunclass'
 		  else:
 			  finel_list[j] = 'linear algebra4'
-  return print(finel_list)
+  return (finel_list)
 
+a=compare_subjects_within_student(sun_class,linear_algebra4)
+print(a)
 
-#finding the max grade btween tow semesters for sun_class
   
